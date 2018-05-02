689d03c21a65acc83f62d127af9cc18fe9fe6e7c
Tony <anthony.c.young18@gmail.com>
2018-05-02 12:16:09 +0100

    mapped express server to index.html

--- tools/devServer.js
+++ tools/devServer.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+})
 
 app.listen(PORT, err => {
   if (err) return console.log('Server start up failed: ', err)
