790c6ebb740b0379375da623c68423955336ccdf
Tony <anthony.c.young18@gmail.com>
2018-05-03 11:00:06 +0100

    updated index.js to use Provider component and copied registerServiceWorker from createReactApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store';
+import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <App />,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
+
+registerServiceWorker();
