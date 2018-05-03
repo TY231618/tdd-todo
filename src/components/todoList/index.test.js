57d24c5424c188c0da0d304f30f05bafa353cc6d
Tony <anthony.c.young18@gmail.com>
2018-05-03 13:52:27 +0100

    added failing test to display text of todo from component props

--- src/components/todoList/index.test.js
+++ src/components/todoList/index.test.js
@@ -4,8 +4,21 @@ import TodoList from './index';
 import '../../../tools/testSetup';
 
 describe('TodoList', () => {
+  
+  const todos = [
+    {
+      id: 1,
+      text: 'A todo'
+    }
+  ];
+
+  const component = shallow(<TodoList todos={todos}/>);
+
   it('should render successfully', () => {
-    const component = shallow(<TodoList />);
     expect(component.exists()).toEqual(true);
+  })
+
+  it('should display a todo when passed as a prop', () => {
+    expect(component.find('.todo-text').text()).toEqual(todos[0].text);
   })
 })
