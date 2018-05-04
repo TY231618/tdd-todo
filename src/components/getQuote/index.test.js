4013fd7b5c9d89aa78d2af818a798744a1f1657f
Tony <anthony.c.young18@gmail.com>
2018-05-04 14:59:50 +0100

    created Quote component to pass render test now have failing test on getQuote prop function

--- src/components/getQuote/index.test.js
+++ src/components/getQuote/index.test.js
@@ -4,8 +4,18 @@ import Quote from './index';
 import '../../../tools/testSetup';
 
 describe('Quote', () => {
+
+  const getQuoteMock = jest.fn();
+
   it('should render successfully', () => {
     const component = shallow(<Quote />);
-    expect(componet.exists()).toEqual(true);
+    expect(component.exists()).toEqual(true);
+  })
+
+  it('should render a quote after calling getQuote on button click', () => {
+    const component = shallow(<Quote getQuote={getQuoteMock}/>);
+    expect(getQuoteMock.mock.calls.length).toEqual(0);
+    component.find('.quote-submit').simulate('click');
+    expect(getQuoteMock.mock.calls.length).toEqual(1);
   })
 })
