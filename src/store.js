bf832f39de77ebaf064f46cf8e7f635c4446f5c1
Tony <anthony.c.young18@gmail.com>
2018-05-11 12:02:22 +0100

    added redux think and updated store to use as middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import todoListApp from './reducers/index';
+
+const middleware = applyMiddleware(thunk);
 
 const reducers = combineReducers({
   todoListApp
 })
 
-export default createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore(reducers, compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
